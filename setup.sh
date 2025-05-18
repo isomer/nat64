9d3de21b2c4ac3f0efcf5a31938e2d1354f9470b
Perry Lorier <git@isomer.meta.net.nz>
2025-05-18 22:59:21 +0100

    Setup proxy arp so I can test to the internet

--- setup.sh
+++ setup.sh
@@ -22,8 +22,13 @@ ip addr add 10.0.0.3/24 metric 10 dev veth0
 ip neigh add fe80::64 lladdr 02:00:00:00:00:64 dev veth0 ||
     ip neigh replace fe80::64 lladdr 02:00:00:00:00:64 dev veth0
 ip neigh add 10.0.0.2 lladdr 02:00:00:00:00:64 dev veth0 ||
-    ip neigh replace 10.0.0.2 lladdr 02:00:00:00:00:64 dev veth0 ||
+    ip neigh replace 10.0.0.2 lladdr 02:00:00:00:00:64 dev veth0
 
 ip route add 64:ff9b:1::/96 via fe80::64 dev veth0
+
+ip route add 192.168.4.4 via 10.0.0.2 dev veth0
+ip neigh add proxy 192.168.4.4 dev wireless
+ip addr add 64:ff9b:1::192.168.4.4 dev veth0
+echo 1 | tee /proc/sys/net/ipv4/conf/veth0/forwarding
 
 cat /sys/kernel/tracing/trace_pipe
